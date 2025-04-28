6eef1c09a7269d191272fd4604977100c272790c
BrandonFanaticProgramm <brandonalexisquintero.sena@gmail.com>
2025-04-28 15:15:17 -0500

    Adding examples about usage of variables

---
+++ 01 -Variables/variables.js
@@ -0,0 +1,17 @@
+//INICIALIZANDO VARIABLE
+var name = "Karl"
+let name_2 = "Bens"
+const name_3 = "Kirk"
+
+{
+    let x = 2
+    let f = 2
+    console.log('Finally result:',x+f) // Block Scope
+}
+
+console.log(x)
+
+// Example using the variables
+console.log("Name 1:", name);
+console.log("Name 2:", name_2);
+console.log("Name 3:", name_3);
