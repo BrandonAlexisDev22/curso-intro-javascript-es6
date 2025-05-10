ce461489f4a6672989d7da32d330f09cebc395ac
BrandonFanaticProgramm <brandonalexisquintero.sena@gmail.com>
2025-05-10 12:07:20 -0500

    Refactor asynchronous example by commenting out unused functions and enhancing callback usage

--- 16-Async-Sync/async.js
+++ 16-Async-Sync/async.js
@@ -1,13 +1,26 @@
-// Simulating an asynchronous operation
-async function sayHello() {
-    return "Hello, world!";
+// // Simulating an asynchronous operation
+// async function sayHello() {
+//     return "Hello, world!";
+// }
+
+// // Async function to call the operation
+// async function greet() {
+//     const message = await sayHello(); // Wait for the result
+//     console.log(message);
+// }
+
+// // Call the async function
+// greet();
+
+function returnData(callback){
+    setTimeout(function(){
+        callback('Ejecutando Codigo');
+    },1000)
 }
 
-// Async function to call the operation
-async function greet() {
-    const message = await sayHello(); // Wait for the result
-    console.log(message);
-}
-
-// Call the async function
-greet();
+console.log("Start of programm") // 1
+returnData(function(data) {
+    console.log(data) // 3
+    console.log("End programm") // 4
+})
+console.log("Async in process") // 2
